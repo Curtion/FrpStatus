9bcef89cac98dd0fa9b4c4a8939642fcc6e22c4a
Curtion <curtion@126.com>
2022-08-07 17:20:43 +0800

    feat: tabbar

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === '服务器') {
+              iconName = focused ? 'laptop-outline' : 'laptop-outline';
+            } else if (route.name === '设置') {
+              iconName = focused ? 'settings-outline' : 'settings-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="服务器" component={HomeScreen} />
+        <Tab.Screen name="设置" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}

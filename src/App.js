dcb2a5e6a7fd82274a56311d36867d768fad3c9d
Curtion <curtion@126.com>
2022-08-09 23:12:20 +0800

    chore: Stack

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import ServerSceen from './views/Server';
 import SettingScreen from './views/Setting';
+
+const Stack = createNativeStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
       <Tab.Navigator
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
@@ -28,6 +32,7 @@ export default function App() {
         <Tab.Screen name="服务器" component={ServerSceen} />
         <Tab.Screen name="设置" component={SettingScreen} />
       </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- src/views/Server.js
+++ src/views/Server.js
@@ -4,7 +4,7 @@ import {Text, View} from 'react-native';
 export default function () {
   return (
     <View>
-      <Text>Settings!</Text>
+      <Text>Server!</Text>
     </View>
   );
 }
